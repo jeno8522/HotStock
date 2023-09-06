9853d623e928b58a632beabbdc841a2c62eb6ab4
윤혜민 <hmyoon97@naver.com>
2023-09-06 17:44:35 +0900

    Feat : component 폴더 및 하위 요소 추가

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>route : 랭킹</div>
+            <div>route : 업종</div>
+            <div>route : 더보기</div>
+        </div>
+    );
+}

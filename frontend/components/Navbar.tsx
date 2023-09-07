28c87a1d54b57d335e23daff4b5b1ae0d9e84b5a
윤혜민 <hmyoon97@naver.com>
2023-09-07 16:48:39 +0900

    Feat : nav-bar link 추가

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,10 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../public/images/hotstocklogo.png";
+
 export default function Navbar() {
     return (
-        <div>
-            <div>Logo</div>
-            <div>route : 랭킹</div>
-            <div>route : 업종</div>
-            <div>route : 더보기</div>
-        </div>
+        <header>
+            <nav className="mx-auto flex justify-between items-center sm:px-10 px-6 py-2 bg-[#2A509D]">
+                <Link href="/">
+                    <Image
+                        src={logo}
+                        alt="logo"
+                        width={90}
+                        className="object-contain"
+                    />
+                </Link>
+                <div>
+                    <Link
+                        className="px-5 text-white hover:text-black"
+                        href="/ranking"
+                    >
+                        랭킹
+                    </Link>
+                    <Link className="px-5 text-white" href="/theme">
+                        업종
+                    </Link>
+                    <Link className="px-5 text-white" href="/aboutus">
+                        더보기
+                    </Link>
+                </div>
+            </nav>
+        </header>
     );
 }

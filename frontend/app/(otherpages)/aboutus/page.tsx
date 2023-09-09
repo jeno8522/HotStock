656fc2402bfc60e41216c79f09516c3e14f44b2d
윤혜민 <hmyoon97@naver.com>
2023-09-10 02:19:21 +0900

    Feat : aboutus 내부 컴포넌트 추가

--- frontend/app/(otherpages)/aboutus/page.tsx
+++ frontend/app/(otherpages)/aboutus/page.tsx
@@ -1,17 +1,9 @@
+import Top from "@/components/aboutus/Top";
+
 const AboutUs = () => {
     return (
         <div>
-            <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
-                <div className="flex-1 pt-36 padding-x">
-                    <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
-                        Hot stock!
-                    </h1>
-
-                    <p className="text-[27px] text-black-100 font-light mt-5">
-                        find keyword jjangjjang jusik chatgi goodgood
-                    </p>
-                </div>
-            </div>
+            <Top />
         </div>
     );
 };

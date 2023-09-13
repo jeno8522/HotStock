c2711b9714d9c6a012f99e342f3fe899336e337a
nhwjik@gmail.com <nhwjik@gmail.com>
2023-09-13 09:24:36 +0900

    Feat : python api 서버 폴더 구조 개선, 테스트 폴더 구조 개선, keyword-themes 연결에 word2vec 적용

--- python-server/flask/service/openai_service.py
+++ python-server/flask/service/openai_service.py
@@ -1,41 +1,38 @@
-from service.theme_service import snapshot
 import json
-
 import openai
 
-
-#FIXME : 실제 서비스에서는 사용할 수 없는 API 키 (테스트 이후 삭제되는 키)
+# FIXME : 실제 서비스에서는 사용할 수 없는 API 키 (테스트 이후 삭제되는 키)
 __test_api_key = "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"
+
 openai.api_key = __test_api_key
 
 command = """
-위 keywords, themes에 대하여 분석할거야.
-각 keyword에 대하여, 관련이 있는 모든 theme들을 (단, keyword는 theme가 아니다) 엮어서 JSON 형태로 반환해줘.
-이 때, 다른 말은 하나도 더하면 안되고, JSON으로만 대답해줘야해.
+너는 최고의 자연어 데이터 전문가야.
+특히 키워드와 키워드의 유사성을 검사하는데 특화되어있지.
+아래의 mapping_data에서 어색하게 연결된 부분을 없애서 Json의 형태로 반환해.
+이때, Json 외에는 다른 어떠한 말도 더 하면 안돼.
 """
 
 
-def make_prompt(keywords:list) :
-        prompt:str = "keywords : \n" + json.dumps(keywords, ensure_ascii=False) + "\n\n" + "themes : \n" + json.dumps(snapshot, ensure_ascii=False) + "\n\n" + command
-        return prompt
+def make_prompt(data: dict):
+    prompt: str = command + "\n\n\n" + json.dumps(data, ensure_ascii=False)
+    return prompt
 
 
 class OpenAIService:
-    
-    def __init__(self):
-        self.content = ""#TODO : 삭제해야할 코드, 테스트를 위한 log 확인용
 
-    
-    def get_themes_of_keywords(self, keywords:list): 
-        content = make_prompt(keywords)
-        messages = [{"role":"user", "content":content}]
-        
+    def __init__(self):
+        pass
+
+    def generate_chat_reply(self, data: str):
+        content = make_prompt(data)
+        messages = [{"role": "user", "content": content}]
+
         completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k-0613",
-            
+            # model="gpt-3.5-turbo-16k-0613",
+            model="gpt-4-0613",
             messages=messages
         )
-        chat_response = completion.choices[0].message.content
-        
-        self.content = content #TODO : 삭제해야할 코드, 테스트를 위한 log 확인용
+        chat_response: str = completion.choices[0].message.content
+
         return chat_response

84b8de22c014e2e93c7e16ae8579454da68b7428
김민우 <jerry142857@naver.com>
2023-09-19 17:44:26 +0900

    feat 워드클라우드 추가

--- frontend/components/Sphere.tsx
+++ frontend/components/Sphere.tsx
@@ -1,69 +1,63 @@
 "use client"
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import TagCloud, {TagCloudOptions} from "TagCloud"; // TagCloud 라이브러리 사용.
+// import { TagCloudOptions } from "TagCloud";
 
-import TagCloud from "TagCloud";
+import "../styles/Sphere.css"
 
-const TextSphere = () => {
+function TextSphere() {
+  const containerRef = useRef(null);
+
   useEffect(() => {
-    return () => {
-      const container = ".tagcloud";
-      const texts: Array<string> = [
-        "HTML5",
-        "CSS3",
-        "JavaScript",
-        "React",
-        "Node.js",
-        "Express",
-        "MongoDB",
-        "Python",
-        "Django",
-        "C",
-        "C++",
-        "Java",
-        "SQL",
-        "Git",
-        "GitHub",
-        "VS Code",
-        "Linux",
-        "Windows",
-        "MacOS",
-        "Arduino",
-        "Raspberry Pi",
-        "AWS",
-        "Heroku",
-        "Firebase",
-        "Google Cloud",
-        "Microsoft Azure",
-        "Adobe Photoshop",
-        "Adobe Illustrator",
-        "Adobe XD",
-        "Adobe Premiere Pro",
-        "Adobe After Effects",
-        "Adobe Lightroom",
-        "Adobe InDesign",
-        "Figma",
+    const container = containerRef.current;
+     const texts: Array<string> = [
+        "반도체",
+        "초전도체",
+        "코로나",
+        "윤석열",
+        "이재명",
+        "주창훈",
+        "삼성전자",
+        "LG전자",
+        "SK하이닉스",
+        "카카오",
+        "소비자 물가 인상",
+        "원유가",
+        "유가급등",
+        "미국",
+        "중국",
+        "일본",
+        "우크라이나",
+        "미국 연준",
+        "바이든",
+        "트럼프",
+        "코로나 백신",
+        "코로나 확진자",
+        "오염수 방류",
+        "미세먼지",
       ];
 
-      const options = {
-        radius: 300,
-        maxSpeed: "fast",
-        initSpeed: "fast",
-        direction: 135,
-        keep: true,
-      };
 
-      TagCloud(container, texts);
+    const options: TagCloudOptions = {
+      radius: 300,
+      maxSpeed: "fast",
+      initSpeed: "fast",
+      // direction: 135,
+      keep: true,
     };
-  }, []);
+
+    if (container) {
+      // 컨테이너가 존재하면 워드 클라우드 초기화
+      TagCloud(container, texts, options);
+    }
+  }, [containerRef]); // containerRef를 의존성 배열에 추가
 
   return (
-    <>
-      <div className="text-shpere">
-        <span className="tagcloud"></span>
-      </div>
-    </>
+    <div className="text-shpere">
+      <span className="tagcloud" ref={containerRef}></span>
+    </div>
   );
-};
+}
 
 export default TextSphere;

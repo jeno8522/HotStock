b2b68577e2dbd71dd54f39b537bf0a195ee6d633
jeno8522 <jeno8522@gmail.com>
2023-09-27 10:02:30 +0900

    Fix : merge bug fixed

--- frontend/components/aboutus/Top.tsx
+++ frontend/components/aboutus/Top.tsx
@@ -2,8 +2,8 @@
 
 import getScrollAnimation from "@/utils/scroll";
 import ScrollAnimationWrapper from "../ScrollAnimationWrapper";
-import { motion } from "framer-motion";
-import React, { useMemo } from "react";
+import {motion} from "framer-motion";
+import React, {useMemo} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import analyticImage from "@/public/images/undraw_Predictive_analytics_re_wxt8.png";
@@ -20,15 +20,33 @@ const Top = () => {
         >
           <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
             <h1
-              className={` text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal`}
+              className={` text-2xl lg:text-3xl xl:text-4xl font-medium text-black-600 leading-normal`}
             >
-              지금 가장 핫한 키워드 보여드림 <strong>Hot Stock</strong>.
+              지금 <span className={`text-orange-500`}>HOT</span> 키워드가 뭐지?
             </h1>
-            <p className="text-black-500 mt-4 mb-6">
-              유사 빅데이터 유사 인공지능이지만 그래도 나름 핀테크임 (진짜)
-            </p>
+            <h1 className="text-2xl-black mt-4 mb-4">
+              실시간 뉴스 기사에서 가장 많이 언급된 키워드들을 확인하세요
+            </h1>
+
             <Link href="/ranking">
-              <button>랭킹 바로가기</button>
+              <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out mb-12">
+                랭킹 바로가기
+              </button>
+            </Link>
+
+            <h1
+              className={` text-2xl lg:text-3xl xl:text-4xl font-medium text-black-600 leading-normal`}
+            >
+              그렇다면, 어떤 주식이{" "}
+              <span className={`text-orange-500`}>HOT</span> 하지?
+            </h1>
+            <h1 className="text-2xl-black mt-4 mb-4">
+              키워드와 관련된 업종, 주식 종목을 확인하세요
+            </h1>
+            <Link href="/theme">
+              <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out mb-12">
+                업종 바로가기
+              </button>
             </Link>
           </div>
           <div className="flex w-full">

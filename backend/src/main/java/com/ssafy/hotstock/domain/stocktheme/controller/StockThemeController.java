90aed1d338de58cd6e456074d0de23aa0fb0282c
김현수 <gustn2615@naver.com>
2023-09-27 13:14:39 +0900

    Feat: 요약 봇 내용 추가 및 주식코드로 테마 이름 가져오기 추가

--- backend/src/main/java/com/ssafy/hotstock/domain/stocktheme/controller/StockThemeController.java
+++ backend/src/main/java/com/ssafy/hotstock/domain/stocktheme/controller/StockThemeController.java
@@ -3,8 +3,10 @@ package com.ssafy.hotstock.domain.stocktheme.controller;
 
 import com.ssafy.hotstock.domain.stocktheme.domain.StockTheme;
 import com.ssafy.hotstock.domain.stocktheme.dto.StockByThemeIdResponseDto;
+import com.ssafy.hotstock.domain.stocktheme.dto.ThemeByStockCodeResponseDto;
 import com.ssafy.hotstock.domain.stocktheme.service.StockThemeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,10 +20,17 @@ public class StockThemeController {
 
     private final StockThemeService stockThemeService;
 
-    @GetMapping("/{themeId}")
+    @GetMapping("theme/{themeId}")
     public ResponseEntity<?> getStockThemeByThemeId(@PathVariable Long themeId) {
         List<StockByThemeIdResponseDto> stockByThemeIdResponseDtos = stockThemeService.getStockByThemeId(themeId);
         return ResponseEntity.ok(stockByThemeIdResponseDtos);
+    }
+
+    @GetMapping("stock/{code}")
+    public ResponseEntity<?> getStockThemeByCodeName(@PathVariable int code) {
+        List<ThemeByStockCodeResponseDto> themeBystockCodeResponseDtoList = stockThemeService.getThemeByStockCode(
+            code);
+        return new ResponseEntity<>(themeBystockCodeResponseDtoList, HttpStatus.OK);
     }
 
 }

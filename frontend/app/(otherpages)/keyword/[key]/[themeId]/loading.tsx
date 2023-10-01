f1e9ace2801a1a53fb997a0c85cc69ecd43c2b3b
윤혜민 <hmyoon97@naver.com>
2023-10-02 03:12:14 +0900

    Design : 키워드 및 주식 상세 페이지 스크롤 css 적용 및 기타 개선

--- frontend/app/(otherpages)/keyword/[key]/[themeId]/loading.tsx
+++ frontend/app/(otherpages)/keyword/[key]/[themeId]/loading.tsx
@@ -6,9 +6,12 @@ export default function Loading() {
   return (
     <div>
       <h2>테마와 종목들을 조회중입니다</h2>
-      {Array.from({ length: len }).map((_, index) => (
-        <StockBarLoading key={index} />
-      ))}
+      <div className="my-3">
+        {Array.from({ length: len }).map((_, index) => (
+          <StockBarLoading key={index} />
+        ))}
+      </div>
+      <div className="border-[#f0f4f9] border-4 border-x-0 border-b-0 my-10" />
     </div>
   );
 }
--- frontend/app/(otherpages)/keyword/[key]/layout.tsx
+++ frontend/app/(otherpages)/keyword/[key]/layout.tsx
@@ -26,7 +26,7 @@ export default async function KeywordDetailLayout({
 
   return (
     <>
-      <div className="max-w-screen-lg px-8 xl:px-10 mt-10 mx-auto">
+      <div className="max-w-screen-lg px-8 xl:px-10 my-10 mx-auto">
         <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
           {/* 왼쪽 이름 탭 */}
           <div className="xl:w-1/5">
@@ -34,13 +34,16 @@ export default async function KeywordDetailLayout({
               {keywordData.keywordContent}
             </div>
             <div>
-              <div>테마선택하실?</div>
               <div className="mt-10">
+                <div className="border-[#c7ced7] border-2 border-x-0 my-1 border-b-0 w-1/4" />
+                <div className="font-bold text-gray-500">
+                  {keywordData.keywordContent}의 테마
+                </div>
                 {!isEmpty ? (
                   keywordData.themeByKeywordIdResponseDtoList.map(
                     (themeList: Theme, index: number) => (
                       <div
-                        className="text-[16px] p-1 my-2 drop-shadow-[1px_1px_1px_rgba(0,0,0,0.2)] bg-sky-50 rounded-md hover:font-bold"
+                        className="text-[16px] p-1 my-2 drop-shadow-[1px_1px_1px_rgba(0,0,0,0.2)] bg-[#f0f4f9] rounded-md hover:font-bold"
                         key={index}
                       >
                         <Link
@@ -53,44 +56,33 @@ export default async function KeywordDetailLayout({
                     )
                   )
                 ) : (
-                  <div>테마 없음</div>
+                  <div>테마가 존재하지 않습니다.</div>
                 )}
               </div>
             </div>
           </div>
           {/* 오른쪽 정보 탭  */}
           <div className="items-center xl:w-4/5">
-            {/* 테마 탭 */}
-            <div>
-              <div className="items-center">
-                {/* 테마 탭 */}
-                {/* <div className="flex">
-                  {keywordData.themeByKeywordIdResponseDtoList.map(
-                    (themeList: Theme, index: number) => (
-                      <div className="text-[20px] pr-5" key={index}>
-                        <Link
-                          href={`/keyword/${keyNumber}/${themeList.themeId}`}
-                          replace
-                        >
-                          {themeList.name}
-                        </Link>
-                      </div>
-                    )
-                  )}
-                </div> */}
-                <div>{children}</div>
+            <div className="keyword_right_box">
+              {/* 테마 탭 */}
+              <div>
+                <div className="items-center">
+                  <div>{children}</div>
+                </div>
               </div>
-            </div>
-            {/* 기사 탭 */}
-            <div className="flex items-center text-[20px]">관련 기사</div>
-            <div>
-              {keywordData.newsByKeywordIdResponseDtoList.map(
-                (news: News, index: number) => (
-                  <div key={index}>
-                    <ArticleCard news={news} />
-                  </div>
-                )
-              )}
+              {/* 기사 탭 */}
+              <div className="text-lg font-bold underline decoration-2 underline-offset-4 decoration-blue-800">
+                {keywordData.keywordContent} 관련 기사
+              </div>
+              <div>
+                {keywordData.newsByKeywordIdResponseDtoList.map(
+                  (news: News, index: number) => (
+                    <div key={index}>
+                      <ArticleCard news={news} />
+                    </div>
+                  )
+                )}
+              </div>
             </div>
           </div>
         </div>

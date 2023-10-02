d5aed590b9b7f4f7bb3b2b6bbbf701d10975c16d
윤혜민 <hmyoon97@naver.com>
2023-10-03 01:32:00 +0900

    Feat : loading 페이지 일부 구현

---
+++ frontend/app/(otherpages)/stock/[stock]/loading.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import darksky from "@/public/images/darksky.jpg";
+
+const Loading = () => {
+  return (
+    <div className="relative">
+      {/* <Image
+        src={darksky}
+        alt="dark-background"
+        layout="fill"
+        objectFit="cover"
+        className="opacity-80"
+      /> */}
+      <div className="flex flex-col w-full h-screen items-center">
+        <div className="loading">
+          <span>L</span>
+          <span>o</span>
+          <span>a</span>
+          <span>d</span>
+          <span>i</span>
+          <span>n</span>
+          <span>g</span>
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;

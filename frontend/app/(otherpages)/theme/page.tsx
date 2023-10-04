d5fdd3e45323568c5df3627e43c6189299e9530e
윤혜민 <hmyoon97@naver.com>
2023-10-05 04:30:40 +0900

    Design : 테마 및 하위 종목 페이지 ui 개선

--- frontend/app/(otherpages)/theme/page.tsx
+++ frontend/app/(otherpages)/theme/page.tsx
@@ -1,11 +1,8 @@
-import {Theme} from "@/types";
-import {fetchThemes} from "@/utils";
-import ThemeBar from "@/components/theme/ThemeBar";
-const Themelist = async ({
-  params,
-}: {
-  params: {key: string; themeId: string};
-}) => {
+import { Theme } from "@/types";
+import { fetchThemes } from "@/utils";
+import { ThemeList, ThemeBar } from "@/components";
+
+const Themelist = async () => {
   const themesResult = await fetchThemes();
 
   const themeList = themesResult;
@@ -17,17 +14,25 @@ const Themelist = async ({
 
   // ----------------------------------------
   return (
-    <div className="items-center">
+    <div className="m-10 lg:px-36 h-fit sm:px-10">
+      <div className="text-[#24364d] font-bold">
+        Hot Stock이 갖고있는 모든 테마들을 보여드려요
+      </div>
+      <div className="text-[#24364d] font-bold">
+        궁금한 테마를 클릭해 테마에 해당되는 주식 종목들을 확인해보세요
+      </div>
       <div>
         {!themeIsEmpty ? (
           <div>
-            {themeList.map((theme) => (
+            <ThemeList themeList={themeList} />
+
+            {/* {themeList.map((theme) => (
               <ThemeBar
                 key={theme.themeId}
                 themeId={theme.themeId}
                 name={theme.name}
               />
-            ))}
+            ))} */}
           </div>
         ) : (
           <div>테마 혹은 이 테마에 해당하는 종목이 존재하지 않습니다.</div>
